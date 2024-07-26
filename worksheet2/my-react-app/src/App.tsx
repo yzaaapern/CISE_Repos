d8f2c263e2e16c69eb60a43ead68c4005e147dab
yza <yzapernia@outlook.com>
2024-07-26 15:05:55 +1200

    feat: changed app.tsx to make an interactive counte

--- worksheet2/my-react-app/src/App.tsx
+++ worksheet2/my-react-app/src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface State {
+  count: number;
 }
 
+class App extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  makeIncrementer = (amount: number) => () =>
+    this.setState((prevState: State) => ({
+      count: prevState.count + amount,
+    }));
+
+  increment = this.makeIncrementer(1);
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button className="increment" onClick={this.increment}>
+          Increment count
+        </button>
+      </div>
+    );
+  }
+}
 export default App;
