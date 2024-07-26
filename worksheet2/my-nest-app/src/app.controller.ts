20dcdccbe7ffb72e7a0d26f0725dd4f0dabd93e1
yza <yzapernia@outlook.com>
2024-07-26 14:34:58 +1200

    build: created new api endpoints and added dotenv and nodemon

--- worksheet2/my-nest-app/src/app.controller.ts
+++ worksheet2/my-nest-app/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ARTICLES } from './dummydata/articles';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/api/articles')
+  getArticles(): any[] {
+    return ARTICLES;
+  }
+
+  @Get('/api/articles/:id')
+  getArticlesById(@Param('id') id: string): any[] {
+    return ARTICLES.find((n) => n._id === id);
   }
 }
--- worksheet2/my-nest-app/src/main.ts
+++ worksheet2/my-nest-app/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  dotenv.config();
+  const PORT = process.env.PORT || 5000;
+  await app.listen(PORT);
 }
 bootstrap();
